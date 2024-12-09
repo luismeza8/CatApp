c84d2195ef25715aa863ee7e5b55df1fc9c19929
luismeza8 <angel841bv@gmail.com>
2024-12-09 15:42:39 -0700

    useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react"
+
 function Body() {
+  const [data, dataSet] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const url = 'https://api.thecatapi.com/v1/images/search?limit=10';
+      try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`)
+        }
+
+        const json = await response.json()
+        console.log(json)
+        dataSet(json)
+        return json
+      } catch(error) {
+        console.error(error.message)
+      }
+    })();
+  }, [])
+
   return(
     <>
       <div className='h-full w-full bg-gray-500 mb-4'>
-        yeap
+        {data.map((cat) => (
+          <div>
+            <img key={cat.id} src={cat.url} width={100}/>
+          </div>
+        ))}
       </div>
     </>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 export default function Sidebar() {
   return(
     <>
-      <ul class="menu mr-4 bg-base-200 rounded-box w-56">
+      <ul className="menu mr-4 bg-base-200 rounded-box w-56">
         <li><a>Item 1</a></li>
         <li><a>Item 2</a></li>
         <li><a>Item 3</a></li>
